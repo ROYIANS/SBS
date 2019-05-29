4a85c7ad0d95cb27c933e5a1774160a0f6721c63
ROYIANS <z1294686101@gmail.com>
2019-05-29 15:49:46 +0800

    update

--- src/main/resource/sqls/data.sql
+++
@@ -1,3 +0,0 @@
-use sbs;
-insert into m_user (U_OPENID, U_GID, U_SESSION_KEY, U_3RD_KEY, U_NICKNAME, U_AVATAR_URL, U_CITY, U_LANGUAGE, U_REGISTER_TIME) VALUES ('null',null,'null','null','admin','null','null','null',now());
-insert into m_group (G_NAME, G_AVATARURL, G_REGISTER_TIME, G_INVITE_CODE, G_ANDMIN) VALUES ('未加入家庭','null',now(),'null',1);
